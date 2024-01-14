6402a2ed781f452192ca88552cb13b8b3fb2661f
Daniel Adigun <105540876+Russoboy@users.noreply.github.com>
2024-01-14 12:49:43 +0100

    Update Decimal-to-Binary-Converter.js
    Update

--- Decimal-to-Binary-Converter.js
+++ Decimal-to-Binary-Converter.js
@@ -3,20 +3,33 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  if (input === 0) {
-    return "";
+  if (input === 0 || input === 1) {
+    return String(input);
   } else {
     return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
 };
 
+const showAnimation = () => {
+  console.log("free");
+  console.log("Code");
+  console.log("Camp");
+};
+
 const checkUserInput = () => {
-  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+  const inputInt = parseInt(numberInput.value);
+
+  if (!numberInput.value || isNaN(inputInt)) {
     alert("Please provide a decimal number");
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(inputInt);
   numberInput.value = "";
 };
 
